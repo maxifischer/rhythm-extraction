c098b662de06b9f750268c734dd683bde3574e1d
Niels Warncke <niels.warncke@gmail.com>
2018-09-20 02:15:56 +0200

    bla

--- src/visualizations_cli.py
+++ src/visualizations_cli.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import madmom
 
 import sys
+
 sys.path.append('../src')
 from preprocessing import RhythmData, SpectroData, MIRData, NORMALIZE_CHANNELS
 from models import *
@@ -21,8 +22,11 @@ from keras import backend as K
 from keras.models import Model
 from sklearn.metrics import log_loss, confusion_matrix
 
+import matplotlib
+
 import pickle
 import pandas as pd
+
 pd.set_option('display.max_columns', 15)
 
 import pdb
@@ -31,131 +35,51 @@ import random
 MUSIC = 1
 SPEECH = 0
 
-RUN_NAME='cluster_cv'
+RUN_NAME = 'cluster_cv'
 
 data_path = {
-                "GTZAN": {
-                    'music_dir': '../data/music_speech/music_wav/',
-                    'speech_dir': '../data/music_speech/speech_wav/'
-                },
-                "columbia-train": {
-                    'music_dir': '../data/columbia_music_speech_corpus/train/music',
-                    'speech_dir': '../data/columbia_music_speech_corpus/train/speech'
-                },
-                "columbia-test": {
-                    'music_dir': '../data/columbia_music_speech_corpus/test/music/vocals',
-                    'speech_dir': '../data/columbia_music_speech_corpus/test/speech'
-                }
-            }
+    "GTZAN": {
+        'music_dir': '../data/music_speech/music_wav/',
+        'speech_dir': '../data/music_speech/speech_wav/'
+    },
+    "columbia-train": {
+        'music_dir': '../data/columbia_music_speech_corpus/train/music',
+        'speech_dir': '../data/columbia_music_speech_corpus/train/speech'
+    },
+    "columbia-test": {
+        'music_dir': '../data/columbia_music_speech_corpus/test/music/vocals',
+        'speech_dir': '../data/columbia_music_speech_corpus/test/speech'
+    }
+}
 
 na = np.newaxis
 
+model_names = ["simple_cnn--linvar", "simple_cnn", "linear--linvar", "linear", "big_cnn--linvar", "big_cnn",
+               "linear--linvar", "linear", "mean_svm"]
 
-model_names =["simple_cnn--linvar", "simple_cnn", "linear--linvar", "linear", "big_cnn--linvar", "big_cnn", "linear--linvar", "linear", "mean_svm"]
 
 # SVM gridsearch values
 def add_svm_grid():
-    svm_models = ['meansvm']# 'patchsvm']
-    C_values     = np.linspace(1, 100, 20)# [96.]  #np.linspace(1., 100, 2)
-    gamma_values = np.logspace(-10, 0, 30)# [1e-6] #np.logspace(-10, 0, 2)
+    svm_models = ['meansvm']  # 'patchsvm']
+    C_values = np.linspace(1, 100, 20)  # [96.]  #np.linspace(1., 100, 2)
+    gamma_values = np.logspace(-10, 0, 30)  # [1e-6] #np.logspace(-10, 0, 2)
 
     for svm_model in svm_models:
         for c in C_values:
             for gamma in gamma_values:
                 model_names.append('{}--{}--{}'.format(svm_model, c, gamma))
-#model_names = []
-#model_names.extend(["simple_cnn--linvar", "simple_cnn", "linear--linvar", "linear"])# "linear", "linear--linvar", "simple_cnn", "simple_cnn--linvar"]
 
 
-
-def cv_experiment(data, model_name, col_test_data, epochs=100, batch_size=8, nfolds=2, nrepetitions=1):
-    """
-
-    :return: ((test accuracy on columbia test data set, f1 score on col test, test acc on colubia test positive class only, test acc on columbia test negative class only), (cv acc on train data, cv f1 score on train data, cv acc on train data pos class only, cv acc on train data neg class only))
-    """
-
-    input_shape = data.X.shape[1:]
-    model = get_model(model_name, input_shape)
-
-    def get_fresh_model(model=model):
-        reset_weights(model)
-        return model
-
-    train_model = lambda model, X, Y: model.fit(X, Y,
-                                        batch_size=batch_size,
-                                        epochs=epochs, verbose=0)
-
-    cvacc = cv(data.X, data.Y, get_fresh_model, train_model, nfolds=nfolds, nrepetitions=nrepetitions)
-
-    test_acc = None
-    if col_test_data is not None: 
-        train_model(model, data.X, data.Y)
-
-        test_acc = evaluate_on_test_set(model, model_name, col_test_data)
-
-    print('cvacc output is of length {}'.format(len(cvacc)))  
-
-    K.clear_session()
-    if len(cvacc) == 5:
-        result = (test_acc[1:], cvacc[1:])
-    else:
-        result = test_acc, cvacc
-    return result
+# model_names = []
+# model_names.extend(["simple_cnn--linvar", "simple_cnn", "linear--linvar", "linear"])# "linear", "linear--linvar", "simple_cnn", "simple_cnn--linvar"]
 
 
-def train_test_experiment(data, model_name, col_test_data, epochs=100, batch_size=8):
-
-    input_shape = data.X.shape[1:]
-    model = get_model(model_name, input_shape)
-
-    def get_fresh_model(model=model):
-        reset_weights(model)
-        return model
-
-    train_model = lambda model, X, Y: model.fit(X, Y,
-                                        batch_size=batch_size,
-                                        epochs=epochs)
-
-    train_model(model, data.X, data.Y)
-    train_acc = model.evaluate(data.X, data.Y)
-    
-    test_acc = evaluate_on_test_set(model, model_name, col_test_data)
-        
-    if len(train_acc) == 5:
-        result = (test_acc[1:], train_acc[1:])
-    else:
-        result = test_acc, train_acc
-
-    K.clear_session()
-
-    return result
-
-def evaluate_on_test_set(model, model_name, col_test_data, return_conf_matrix=False):
-    
-    try:
-
-        if return_conf_matrix:
-            test_predictions  = model.predict(col_test_data.X)
-            test_acc = confusion_matrix(col_test_data.Y, test_predictions)
-        else:
-            test_acc  = model.evaluate(col_test_data.X, col_test_data.Y)
-    except ValueError:
-        # if the test set has a different time length then the train set, try to reshape the model and test then
-        test_input_shape = col_test_data.X.shape[1:]
-        model_weights = model.get_weights()
-        reshaped_model = reshape_keras_conv_input(model_name, test_input_shape, model_weights)
-
-        if return_conf_matrix:
-            test_predictions = reshaped_model.predict(col_test_data.X)
-            test_acc = confusion_matrix(col_test_data.Y, test_predictions)
-        else:
-            test_acc = reshaped_model.evaluate(col_test_data.X, col_test_data.Y)
-
-    return test_acc
 
 """
 The following stuff happens for trained models
 """
+
+
 def get_trained_model(data, model_name, epochs=100, batch_size=8):
     input_shape = list(data.X.shape[1:])
     model = get_model(model_name, input_shape)
@@ -176,8 +100,10 @@ def get_trained_model(data, model_name, epochs=100, batch_size=8):
         train_model(model, data.X, data.Y)
         try:
             model.save(weight_path)
-        except: pass
+        except:
+            pass
     return model
+
 
 def get_accuracy(data, model_name, model, col_test_data):
     r = cv_experiment(data, model_name, col_test_data)
@@ -192,11 +118,13 @@ def important_channels(data, model_name, model, col_test_data):
     # TODO
     pass
 
+
 def visualize_prediction_over_time(data, model_name, model, music_sample, speech_sample):
     result_dir = "../results/{}--{}".format(data.__class__.__name__, model_name)
     # plot for some samples the prediction over time, and also for a transition of music to speech
     # including correlation
     visualize.prediction_over_time(music_sample, speech_sample, model, result_dir)
+
 
 def analyze_error(data, model_name, model, col_test_data):
     result_dir = "../results/{}--{}".format(data.__class__.__name__, model_name)
@@ -205,10 +133,11 @@ def analyze_error(data, model_name, model, col_test_data):
     # TODO
     pass
 
+
 def visualize_filter(data, model_name, model, col_test_data, music_sample, speech_sample):
     original_len = music_sample.shape[1]
     target_len = model.input_shape[2]
-    T_ORIGINAL = 30*original_len/target_len
+    T_ORIGINAL = 30 * original_len / target_len
     if original_len < target_len:
         m, s = np.zeros(model.input_shape[1:]), np.zeros(model.input_shape[1:])
         m[:, :original_len] = music_sample
@@ -285,14 +214,15 @@ def plot_channel_activation(music, speech, export_path, seconds=15):
     os.makedirs(export_path, exist_ok=True)
 
     for channel in range(num_channels):
-        plt.plot(time, music[0,:, channel], label="Music")
-        plt.plot(time, speech[0,:, channel], label="Speech")
+        plt.plot(time, music[0, :, channel], label="Music")
+        plt.plot(time, speech[0, :, channel], label="Speech")
         plt.xlabel("Time/s")
         plt.ylabel("Activation")
         plt.title("Channel {}".format(channel))
         plt.legend()
-        plt.savefig(export_path+"/channel-{}-sample.png".format(channel))
+        plt.savefig(export_path + "/channel-{}-sample.png".format(channel))
         plt.clf()
+
 
 def plot_channel_activations(all_music, all_speech, export_path, seconds=15):
     num_channels = all_speech.shape[-1]
@@ -302,18 +232,19 @@ def plot_channel_activations(all_music, all_speech, export_path, seconds=15):
     for channel in range(num_channels):
         label = "Music"
         for music in all_music:
-            plt.plot(time, music[0,:, channel], color="green", label=label)
-            label=None # do only one music label, not multiples
+            plt.plot(time, music[0, :, channel], color="green", label=label)
+            label = None  # do only one music label, not multiples
         label = "Speech"
         for speech in all_speech:
-            plt.plot(time, speech[0,:, channel], color="red", label=label)
-            label=None
+            plt.plot(time, speech[0, :, channel], color="red", label=label)
+            label = None
         plt.xlabel("Time/s")
         plt.ylabel("Activation")
         plt.title("Channel {}".format(channel))
         plt.legend()
-        plt.savefig(export_path+"/channel-{}-all-samples.png".format(channel))
+        plt.savefig(export_path + "/channel-{}-all-samples.png".format(channel))
         plt.clf()
+
 
 def plot_channel_activations_mv(all_music, all_speech, export_path, seconds=15):
     num_channels = all_speech.shape[-1]
@@ -327,17 +258,17 @@ def plot_channel_activations_mv(all_music, all_speech, export_path, seconds=15):
         speech_v = np.var(all_speech[:, 0, :, channel], axis=0)
 
         plt.plot(time, music_m, label="Music", color="green")
-        plt.plot(time, music_m+music_v, "--", color="green")
-        plt.plot(time, music_m-music_v, "--", color="green")
+        plt.plot(time, music_m + music_v, "--", color="green")
+        plt.plot(time, music_m - music_v, "--", color="green")
         plt.plot(time, speech_m, label="Speech", color="red")
-        plt.plot(time, speech_m+speech_v, "--", color="red")
-        plt.plot(time, speech_m-speech_v, "--", color="red")
+        plt.plot(time, speech_m + speech_v, "--", color="red")
+        plt.plot(time, speech_m - speech_v, "--", color="red")
 
         plt.xlabel("Time/s")
         plt.ylabel("Activation")
         plt.title("Channel {}".format(channel))
         plt.legend()
-        plt.savefig(export_path+"/channel-{}-mv.png".format(channel))
+        plt.savefig(export_path + "/channel-{}-mv.png".format(channel))
         plt.clf()
 
 
@@ -359,8 +290,8 @@ def visualize_channel_activation():
             music_sample = random.choice(music)
             speech_sample = random.choice(speech)
 
-            #plot_channel_activation(music_sample, speech_sample, "../results/{}".format(prepr_name))
-            #plot_channel_activations(music, speech, "../results/{}".format(prepr_name))
+            # plot_channel_activation(music_sample, speech_sample, "../results/{}".format(prepr_name))
+            # plot_channel_activations(music, speech, "../results/{}".format(prepr_name))
             plot_channel_activations_mv(music, speech, "../results/{}".format(prepr_name))
 
 
@@ -371,27 +302,24 @@ def analyze_trained_models():
     for data_name, kwargs in data_path.items():
 
         if data_name == "columbia-test": continue  # don't use the test set for training
-        for Preprocessor in [SpectroData]: #[RhythmData, MIRData, SpectroData]:
+        for Preprocessor in [SpectroData]:  # [RhythmData, MIRData, SpectroData]:
             prepr_name = Preprocessor.__name__
             data = Preprocessor(**kwargs)
 
             col_test_data = Preprocessor(**data_path["columbia-test"])
 
-            music_sample    = random.choice(col_test_data.X[col_test_data.Y == MUSIC])
-            speech_sample   = random.choice(col_test_data.X[col_test_data.Y != MUSIC])
+            music_sample = random.choice(col_test_data.X[col_test_data.Y == MUSIC])
+            speech_sample = random.choice(col_test_data.X[col_test_data.Y != MUSIC])
 
             print("Col test data \nX: {}, Y: {}".format(col_test_data.X.shape, col_test_data.Y.shape))
-
 
             for model_name in model_names:
                 print("\n\n-------\nAnalyze {} on {} - {}".format(model_name, data_name, prepr_name))
 
                 print("music sample: ", music_sample.shape)
 
-
-
                 model = get_trained_model(data, model_name)
-                #get_accuracy(data, model_name, model, col_test_data)
+                # get_accuracy(data, model_name, model, col_test_data)
                 visualize_prediction_over_time(data, model_name, model, music_sample, speech_sample)
                 analyze_error(data, model_name, model, col_test_data)
                 important_channels(data, model_name, model, col_test_data)
@@ -403,7 +331,7 @@ def important_channels():
     SPEECH = 0
 
     results = pd.DataFrame(columns=["model", "channel", "preprocesssing", "effect_acc", "effect_kldiv"])
-    row_id = 0 # counter for where to store the results
+    row_id = 0  # counter for where to store the results
 
     for data_name, kwargs in data_path.items():
 
@@ -425,13 +353,13 @@ def important_channels():
                 model = get_trained_model(data, model_name)
 
                 def acc(p):
-                    return np.mean((p>0.5)==(data.Y>0.5))
+                    return np.mean((p > 0.5) == (data.Y > 0.5))
 
                 y = model.predict(data.X)
 
                 for channel in range(data.X.shape[-1]):
                     X_ = np.array(data.X)
-                    X_[:,:,:,channel] = 0
+                    X_[:, :, :, channel] = 0
                     p = model.predict(X_)
                     values = [model_name, channel, prepr_name, acc(p), log_loss(y, p)]
                     results[row_id] = values
@@ -441,16 +369,13 @@ def important_channels():
     print('...saved results')
 
 
-
-
-
-
 def run_on_all(experiment):
-    cols = ['data_name','prepr_name','model_name', 'param_c', 'param_gamma', 'param_linvar', 'test_acc', 'test_f1', 'test_acc_pc', 'test_acc_nc', 'cv_acc', 'cv_f1', 'cv_acc_pc', 'cv_acc_nc']
+    cols = ['data_name', 'prepr_name', 'model_name', 'param_c', 'param_gamma', 'param_linvar', 'test_acc', 'test_f1',
+            'test_acc_pc', 'test_acc_nc', 'cv_acc', 'cv_f1', 'cv_acc_pc', 'cv_acc_nc']
     results = pd.DataFrame(columns=cols)
     for data_name, kwargs in data_path.items():
 
-        if data_name == "columbia-test": continue # don't use the test set for training
+        if data_name == "columbia-test": continue  # don't use the test set for training
         for Preprocessor in [RhythmData, MIRData, SpectroData]:
             prepr_name = Preprocessor.__name__
             data = Preprocessor(**kwargs)
@@ -463,7 +388,7 @@ def run_on_all(experiment):
                 split_model = model_name.split('--')
                 print('finished cv, result:')
                 print(result)
-                
+
                 if split_model[-1] == 'linvar':
                     param_linvar = True
                 else:
@@ -479,15 +404,15 @@ def run_on_all(experiment):
                 df_vals.extend(flattened[0:8])
                 results = results.append(pd.DataFrame(dict(zip(cols, df_vals)), index=[0]), ignore_index=True)
 
-                #test_acc, test_f1 = result[0][0], result[0][1]
-                #cv_acc, cv_f1 = result[1][0], result[1][1]
+                # test_acc, test_f1 = result[0][0], result[0][1]
+                # cv_acc, cv_f1 = result[1][0], result[1][1]
 
-                #df_vals = [data_name, prepr_name, model_name, param_c, param_gamma, param_linvar, test_acc, test_f1, cv_acc, cv_f1]
-                #results.loc[results.shape[0]] = df_vals
+                # df_vals = [data_name, prepr_name, model_name, param_c, param_gamma, param_linvar, test_acc, test_f1, cv_acc, cv_f1]
+                # results.loc[results.shape[0]] = df_vals
 
-                #flattened = [val for sublist in result for val in sublist]
-                #df_vals.extend(flattened[0:4])
-                #results = results.append(pd.DataFrame(dict(zip(cols, df_vals)), index=[0]), ignore_index=True)
+                # flattened = [val for sublist in result for val in sublist]
+                # df_vals.extend(flattened[0:4])
+                # results = results.append(pd.DataFrame(dict(zip(cols, df_vals)), index=[0]), ignore_index=True)
     return results
 
 
@@ -521,7 +446,6 @@ if __name__ == "__main__":
         merged.to_csv("results/merged.csv", index=False)
         open_csv = "results/merged.csv"
 
-
     if cmd == "open-csv":
         open_csv = sys.argv[2]
 
@@ -531,9 +455,9 @@ if __name__ == "__main__":
 
         results = results.dropna(subset=["cv_acc"])
 
-        #results = results.drop(
+        # results = results.drop(
         #    results.loc[results.cv_acc].index
-        #)
+        # )
 
         models = results["model_name"].unique()
         datasets = results["data_name"].unique()
@@ -548,14 +472,14 @@ if __name__ == "__main__":
         print(' -------- ')
 
         best_overall_run = results.iloc[results[["cv_acc"]].idxmax()]
-        print("highest cv_accuracy:",  best_overall_run)
-
+        print("highest cv_accuracy:", best_overall_run)
 
         print("Best run per dataset:")
         best_run_per_dataset = results.groupby("data_name")["cv_acc"].idxmax()  # .apply(np.argmax)
         for row in best_run_per_dataset:
             v = results.iloc[row]
-            print("{}: {} on {} with {} ({})".format(v["data_name"], v["model_name"], v["prepr_name"], v["cv_acc"], "normalized" if v["is_normalized"]==1 else "unnorm."))
+            print("{}: {} on {} with {} ({})".format(v["data_name"], v["model_name"], v["prepr_name"], v["cv_acc"],
+                                                     "normalized" if v["is_normalized"] == 1 else "unnorm."))
             print(v)
             print("\n")
 
@@ -563,7 +487,7 @@ if __name__ == "__main__":
         print('Best model: {}'.format(best_overall_run["model_name"]))
         print('(On {})'.format(best_overall_run["prepr_name"]))
         print('Model Selection Accuracy: {}'.format(best_overall_run["cv_acc"]))
-        #print(results)
+        # print(results)
     elif cmd == "analyze_trained":
         analyze_trained_models()
     elif cmd == "important_channels":
@@ -587,5 +511,5 @@ if __name__ == "__main__":
                      channel_activation - plots the activation of channels for MIR and Spectro data
                      is_normalized <path to csv> <value> - set the new column 'is_normalized' is <value>, to specify if the file was generated with normalized channels
         """.format(sys.argv[0]))
-        print("Unknown cmd: "+cmd)
+        print("Unknown cmd: " + cmd)
 
